df8e622bd47e756ca3aaa944a202bd492df5ea9b
Shruti Khetan <skhetan@hawk.iit.edu>
2017-02-20 18:06:40 -0600

    Update blacksholes and greeks.py

--- blacksholes and greeks.py
+++ blacksholes and greeks.py
@@ -1,6 +1,6 @@
+#Pricing engine module that calculates Option prices and Greeks with black scholes
+#The Black Scholes Formula
 """ 
-#pricing engine module that calculates Option prices and Greeks with black scholes
-# The Black Scholes Formula
 # CallPutFlag - This is set to 'c' for call option, anything else for put
 # S - Stock price
 # K - Strike price
@@ -19,9 +19,11 @@ def BlackScholes(CallPutFlag,S,K,T,r,d,v):
   else:
     return K*exp(-r*T)*norm.cdf(-d2)-S*exp(-d*T)*norm.cdf(-d1)
 
+
+  #Greeks in the Blacksholes
+  #Calculating the partial derivatives for a Black Scholes Option (Call)
+  
 """
-#Greeks in the Blacksholes
-#Calculating the partial derivatives for a Black Scholes Option (Call)
 Return:
 Delta: partial wrt S
 Gamma: second partial wrt S
@@ -39,7 +41,6 @@ def Black_Scholes_Greeks_Call(S, K, r, v, T, d):
   Vega_Call = S * T_sqrt*norm.pdf(d1)
   Rho_Call = K*T*exp(-r*T)*norm.cdf(d2)
     return Delta_Call, Gamma_Call, Theta_Call, Vega_Call, Rho_Call
-#print Black_Scholes_Greeks_Call(100, 100, 0.005, 0.06, 0.4, 0)
 
 #Calculating the partial derivatives for a Black Scholes Option (Put)
 
@@ -51,10 +52,3 @@ def Black_Scholes_Greeks_Put(S, K, r, v, T, d):
   Vega_Put = S * T_sqrt * norm.pdf(d1)
   Rho_Put = -K*T*exp(-r*T) * norm.cdf(-d2)
     return Delta_Put, Gamma_Put, Theta_Put, Vega_Put, Rho_Put
-#print Black_Scholes_Greeks_Put(100, 100, 0.005, 0.06, 0.4, 0)
-
-
-
-
-
-
